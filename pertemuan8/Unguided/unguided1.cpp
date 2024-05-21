f2b33d41ba2e1421f79fc174684b3da4f892dab9
amandawindhu <amandawindi073@gmail.com>
2024-05-21 14:24:57 +0700

    add_modul

--- pertemuan8/Unguided/unguided1.cpp
+++ pertemuan8/Unguided/unguided1.cpp
@@ -1,5 +1,8 @@
 // Oleh Amanda Windhu Gustyas_2311102121
 #include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <string>
 #include <algorithm>
 
 using namespace std;
@@ -7,40 +10,46 @@ using namespace std;
 string kalimat;
 char cari;
 
-// Fungsi untuk melakukan pencarian biner pada string yang telah diurutkan
-bool binary_search(const string& kalimat, char cari) {
- int awal = 0, akhir = kalimat.length() - 1;
- while (awal <= akhir) {
- int tengah = (awal + akhir) / 2; // Menghitung indeks tengah
- if (kalimat[tengah] == cari) {
- return true; // Huruf ditemukan
- } else if (kalimat[tengah] < cari) {
- awal = tengah + 1; // Cari di bagian kanan
- } else {
- akhir = tengah - 1; // Cari di bagian kiri
- }
- }
- return false; // Huruf tidak ditemukan
+// Fungsi untuk melakukan binary search pada string
+void binarySearch(){
+    int awal, akhir, tengah;
+    bool b_flag = false; // Flag untuk menandakan apakah data ditemukan
+    awal = 0;
+    akhir = kalimat.length() - 1;
+    while (!b_flag && awal <= akhir)
+    {
+        tengah = (awal + akhir)/2;
+        if (kalimat[tengah] == cari)
+        {
+            b_flag = true;  // Data ditemukan
+        } else if (kalimat[tengah] <  cari)
+        {
+            awal = tengah +1; // Cari di bagian kanan
+        } else {
+            akhir = tengah -1; // Cari di bagian kiri
+        }
+    }
+    if (b_flag){
+        cout << "\nData ditemukan pada index ke- " << tengah <<endl;
+    } else {
+        cout << "\nDATA TIDAK DITEMUKAN" << endl;
+    }
 }
-int main() {
-    cout << "--- MENCARI HURUF ---\n";
-    cout << "Masukkan kalimat: ";
-    getline(cin, kalimat);
-    cout << "Masukkan huruf yang ingin dicari: ";
-    cin >> cari;
-    cari = tolower(cari); // Mengubah huruf yang dicari menjadi huruf kecil
 
-    // Mengurutkan kalimat agar bisa dilakukan pencarian biner
+int main() {
+    cout << "---Cari Huruf---"<<endl;
+    cout << "Masukkan kalimat : ";
+    getline(cin, kalimat);
+    cout << "Masukkan huruf yang ingin dicari : ";
+    cin >> cari;
+
+    // Mengurutkan kalimat
     sort(kalimat.begin(), kalimat.end());
 
-    // Melakukan pencarian biner
-    bool ditemukan = binary_search(kalimat, cari);
+    cout << "\nKalimat diurutkan : " << kalimat << endl;
 
-    // Menampilkan hasil pencarian
-    if (ditemukan) {
-    cout << "Huruf " << cari << " ditemukan." << endl;
-    } else {
-    cout << "Huruf " << cari << " tidak ditemukan." << endl;
-        }
+    cout << "\nBinary Search untuk mencari huruf '" << cari << "' dalam kalimat '" << kalimat << "' : ";
+    binarySearch(); // Memanggil fungsi untuk melakukan pencarian biner
+    _getche(); // Menunggu pengguna menekan tombol sebelum keluar
     return 0;
 }
